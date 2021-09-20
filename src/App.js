1db82265b81af8b61a11bac43cf11e97c0fecf34
ekaterina <strix1112@gmail.com>
2021-09-20 13:31:00 +0300

    homework-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import styles from './App.module.css';
+import {Message} from './components/Message';
+
+const text = "Message Component text";
 
 function App() {
   return (
     <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message textMessage={text}></Message>
     </div>
   );
 }
